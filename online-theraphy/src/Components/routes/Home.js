3107383c7bd25cc2bd4c251e88e8cb0ec52e3495
mehmetcandinc <dincmehmetcann@gmail.com>
2023-01-28 13:10:16 +0300

    image design

--- online-theraphy/src/Components/routes/Home.js
+++ online-theraphy/src/Components/routes/Home.js
@@ -3,7 +3,40 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className="main">
+      <div className="home">
+        <div className="content">
+          <p>Call us</p>
+          <p>1-800-123-4567</p>
+          <p>Because Money</p>
+          <p>Doesn't come with instructions</p>
+          <button href="/home" className="button">
+            Free Consultation
+          </button>
+        </div>
+      </div>
+      <div className="home">
+        <div className="content">
+          <p>Call us</p>
+          <p>1-800-123-4567</p>
+          <p>Because Money</p>
+          <p>Doesn't come with instructions</p>
+          <button href="/home" className="button">
+            Free Consultation
+          </button>
+        </div>
+      </div>
+      <div className="home">
+        <div className="content">
+          <p>Call us</p>
+          <p>1-800-123-4567</p>
+          <p>Because Money</p>
+          <p>Doesn't come with instructions</p>
+          <button href="/home" className="button">
+            Free Consultation
+          </button>
+        </div>
+      </div>
       <div className="home">
         <div className="content">
           <p>Call us</p>
